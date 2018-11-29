8764dcc75f835ca15992a165b2e073adfdd789bb
Electroshockist <ericonfire2@yahoo.com>
2018-11-29 17:11:05 -0500

    Added traps and player damage

---
+++ Assets/Scripts/Trap.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour {
+    float DamageValue = 10;
+
+    private void OnTriggerEnter(Collider collision) {
+
+        Debug.Log(tag);
+        if (collision.gameObject.tag == "Player") collision.gameObject.GetComponent<Character>().Damage(DamageValue);
+
+    }
+}
